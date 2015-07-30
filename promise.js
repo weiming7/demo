4312f4c42b184cc19dcb3a4aefbc3b9d63155709
乔治 <1975657371@qq.com>
2015-07-30 20:06:47 +0800

    express promise

--- promise.js
+++ promise.js
@@ -6,7 +6,7 @@
  *  George
  *  2015/7/30 18:12
  */
-//var Q = require('q');
+var Q = require('q');
 var fs = require('fs');
 
 //function fs_readFile(file){
@@ -25,6 +25,30 @@ var fs = require('fs');
 //fs_readFile('1.txt')
 //    .then(function(data){console.log(data.toString())})
 //    .fail(function(err){console.log(err)});
+
+
+
+function getOne(num){
+    return Q.promise(function (resolve, reject) {
+        var data = num;
+        resolve(data);
+    })
+}
+
+function getTwo(num2){
+    return Q.promise(function (resolve, reject) {
+        var data = num2;
+        resolve(data);
+    })
+}
+
+getOne(100).then(function (data) {
+    console.log(data);
+    return getTwo(data);
+}).then(function(data){
+    console.log(data);
+})
+
 
 
 
@@ -77,49 +101,84 @@ var fs = require('fs');
 
 
 
-var val = 1;
+//var val = 1;
+//
+//// 我们假设step1, step2, step3都是ajax调用后端或者是
+//// 在Node.js上查询数据库的异步操作
+//// 每个步骤都有对应的失败和成功处理回调
+//// 需求是这样，step1、step2、step3必须按顺序执行
+//function step1(resolve, reject) {
+//    console.log('步骤一：执行');
+//    if (val >= 1) {
+//        resolve('Hello I am No.1');
+//    } else if (val === 0) {
+//        reject(val);
+//    }
+//}
+//
+//function step2(resolve, reject) {
+//    console.log('步骤二：执行');
+//    if (val === 1) {
+//        resolve('Hello I am No.2');
+//    } else if (val === 0) {
+//        reject(val);
+//    }
+//}
+//
+//function step3(resolve, reject) {
+//    console.log('步骤三：执行');
+//    if (val === 1) {
+//        resolve('Hello I am No.3');
+//    } else if (val === 0) {
+//        reject(val);
+//    }
+//}
+//
+//new Promise(step1).then(function(val){
+//    console.info(val);
+//    return new Promise(step2);
+//}).then(function(val){
+//    console.info(val);
+//    return new Promise(step3);
+//}).then(function(val){
+//    console.info(val);
+//    return val;
+//}).then(function(val){
+//    console.info(val);
+//    return val;
+//});
+//
+//
+//
+//Movie.findById(id, function(err, movie) {
+//    Category.find({}, function(err, categories) {
+//        res.render('admin', {
+//            title: 'imooc 后台更新页',
+//            movie: movie,
+//            categories: categories
+//        })
+//    })
+//});
+//
+//
+//var id = req.params.id;
+//
+//new Promise(getMovie).then(function(val){
+//    console.info(val);
+//    return new Promise(getCateory);
+//}).then(function(val){
+//    console.info(val);
+//})
+//
+//function getMovie(resolve,reject){
+//    Movie.findById(id,function(err,movie){
+//        resolve(movie);
+//    });
+//}
+//
+//function getCateory(resolve,reject){
+//    Category.find({}, function(err, categories) {
+//        resolve(categories);
+//    });
+//}
 
-// 我们假设step1, step2, step3都是ajax调用后端或者是
-// 在Node.js上查询数据库的异步操作
-// 每个步骤都有对应的失败和成功处理回调
-// 需求是这样，step1、step2、step3必须按顺序执行
-function step1(resolve, reject) {
-    console.log('步骤一：执行');
-    if (val >= 1) {
-        resolve('Hello I am No.1');
-    } else if (val === 0) {
-        reject(val);
-    }
-}
-
-function step2(resolve, reject) {
-    console.log('步骤二：执行');
-    if (val === 1) {
-        resolve('Hello I am No.2');
-    } else if (val === 0) {
-        reject(val);
-    }
-}
-
-function step3(resolve, reject) {
-    console.log('步骤三：执行');
-    if (val === 1) {
-        resolve('Hello I am No.3');
-    } else if (val === 0) {
-        reject(val);
-    }
-}
-
-new Promise(step1).then(function(val){
-    console.info(val);
-    return new Promise(step2);
-}).then(function(val){
-    console.info(val);
-    return new Promise(step3);
-}).then(function(val){
-    console.info(val);
-    return val;
-}).then(function(val){
-    console.info(val);
-    return val;
-});
